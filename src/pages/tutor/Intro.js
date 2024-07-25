5984c86ec615283f536297ab92bc4fe03433fc59
Asiya Batool <asiya.batool987@gmail.com>
2024-07-25 19:31:24 +0500

    sidebar

--- src/pages/tutor/Intro.js
+++ src/pages/tutor/Intro.js
@@ -1,19 +1,15 @@
 import Intro from "../../components/tutor/Intro";
 import TutorLayout from "../../layouts/TutorLayout";
-import TabInfoVideoToast from '../../components/common/TabInfoVideoToast'
-import Dashboard from '../../layouts/Dashboard'
-
 
 const TutorIntro = () => {
-    return (
-        <Dashboard >
-                {/* // <TutorLayout  > */}
-                <Intro />
-                {/* <TabInfoVideoToast video={VIDEO} /> */}
-            {/* // </TutorLayout> */}
-
-            </Dashboard>
-    );
-}
+  return (
+    <TutorLayout>
+      <Intro />
+      {/* // <Dashboard > */}
+      {/* <TabInfoVideoToast video={VIDEO} /> */}
+      {/* // </Dashboard> */}
+    </TutorLayout>
+  );
+};
 
 export default TutorIntro;
