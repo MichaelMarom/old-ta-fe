7344e1263bb02f489c68a73147608232dda1062f
Asiya Batool <asiya.batool987@gmail.com>
2025-01-11 19:42:42 +0500

    updated code

--- src/components/common/Divider.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Divider = ({ background="#d9d9d9",height="1px", width="100%", margin="10px"}) => {
-  return (
-    <div style={{height, background,margin, width}}></div>
-  )
-}
-
-export default Divider
